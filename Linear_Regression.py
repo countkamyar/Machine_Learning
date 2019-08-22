992238ed51aefa4ef4a555500c516e72ff6032e1
Kamyar <kamyar21rostami@gmail.com>
2019-08-22 10:17:11 +0400

    beta_algorithm_linear_regressioncd

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -1,14 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # creating our training set
 class Dataset:
     @staticmethod
     def create_dataset_x():
-        a = np.array([[1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0]])
+        a = np.array([[0.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0]])
         return a
     @staticmethod
     def create_dataset_y():
-        b = np.array([[1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0]])
+        b = np.array([[5.0,9.0,11.0,20.0,15.0,5.0,19.0,21.0,23.0,25.0]])
         return b
 
 
@@ -17,7 +18,9 @@ class Dataset:
 class LinearRegression:
     ys=np.ndarray(shape=(1,10),dtype=float)
     xs=np.ndarray(shape=(1,10),dtype=float)
-    epochs=100
+    epochs=200
+    t0_values=[]
+    t1_values=[]
     def __init__(self,  t0,  t1,  alpha):
         self.t0=t0
         self.t1=t1
@@ -45,13 +48,17 @@ class LinearRegression:
         print('theta values:')
         print('theta0: ',self.t0,'theta1: ',self.t1)  
         if(self.epochs==0):
-            pass
+            plt.plot(self.t0_values,self.t1_values)
+            plt.show()
         else:
+            self.t0_values.append(self.t0)
+            self.t1_values.append(self.t1)
             self.epochs-=1
             self.ignit()    
-
+        
   
 
 lr=LinearRegression(0,1,1)
 lr.ignit()
+
 
